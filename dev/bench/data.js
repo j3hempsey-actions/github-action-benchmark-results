19d7a62572310cb7ad26801703e07b339d70f1eb
github-action-benchmark <github@users.noreply.github.com>
2022-12-09 23:17:27 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 199abc83cdf9b37f6180294f507ead121520acf8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1670627844586,
+  "lastUpdate": 1670627847032,
   "repoUrl": "https://github.com/j3hempsey-actions/github-action-benchmark",
   "entries": {
     "Go Benchmark": [
@@ -154,6 +154,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00004781972637552326",
             "extra": "mean: 2.632505633424092 msec\nrounds: 371"
+          }
+        ]
+      }
+    ],
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jhempsey33@gmail.com",
+            "name": "Justin Hempsey",
+            "username": "j3hempsey"
+          },
+          "committer": {
+            "email": "jhempsey33@gmail.com",
+            "name": "Justin Hempsey",
+            "username": "j3hempsey"
+          },
+          "distinct": true,
+          "id": "199abc83cdf9b37f6180294f507ead121520acf8",
+          "message": "Add job summary example",
+          "timestamp": "2022-12-09T15:13:44-08:00",
+          "tree_id": "bb864c08699a7429567ecf47e65415bb100c35d4",
+          "url": "https://github.com/j3hempsey-actions/github-action-benchmark/commit/199abc83cdf9b37f6180294f507ead121520acf8"
+        },
+        "date": 1670627846601,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Sample.Benchmarks.Fib10",
+            "value": 29.372251195566996,
+            "unit": "ns",
+            "range": "± 0.00857084751914548"
+          },
+          {
+            "name": "Sample.Benchmarks.Fib20",
+            "value": 54.74496523539225,
+            "unit": "ns",
+            "range": "± 0.06493650635895815"
           }
         ]
       }
